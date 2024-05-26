e4cc1c196225601e3efc7737b14ba335cd3b7824
João Lemes <joaolemes17@hotmail.com>
2024-05-26 16:51:00 -0400

    feat: new UI/UX 1.0 is finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { ModalProvider } from "./context/MsgContext";
-
 import AppRoutes from "./routes";
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <BrowserRouter>
       <ModalProvider>
         <section className="app" style={{ height: "100vh" }}>
+          <Logo />
           <AppRoutes />
         </section>
       </ModalProvider>

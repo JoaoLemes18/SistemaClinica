f5b4d2b4a446fc94280b28e01f2456704319d955
João Lemes <joaolemes17@hotmail.com>
2024-10-18 01:24:04 -0400

    commit

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -6,14 +6,13 @@ import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { toast } from "react-toastify";
-import { useDispatch } from "react-redux"; // Importando useDispatch
-import { setUser } from "../../store/user/userSlice"; // Importando setUser
+import { useDispatch } from "react-redux";
+import { setUser } from "../../store/user/userSlice";
 import "./styles.scss";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch(); // Inicializando dispatch
-
+  const dispatch = useDispatch();
   const [form, setForm] = useState({
     cod_prof: "",
     senha_prof: "",
@@ -37,17 +36,15 @@ const LoginPage = () => {
       if (userData.cod_prof && userData.nome_prof) {
         localStorage.setItem("userData", JSON.stringify(userData));
 
-        // Atualiza o Redux com os dados do usuário, incluindo tipo_prof
         dispatch(
           setUser({
             cod_prof: userData.cod_prof,
             nome_prof: userData.nome_prof,
-            cod_espec: userData.cod_espec, // Presumindo que também esteja na resposta
-            tipo_prof: userData.tipo_prof, // Adicionando tipo_prof
+            cod_espec: userData.cod_espec,
+            tipo_prof: userData.tipo_prof,
           })
         );
 
-        // Redireciona para a Home com tipo_prof na URL
         navigate(`/home?tipo_prof=${userData.tipo_prof}`);
       } else {
         toast.error("Dados de usuário incompletos na resposta do servidor.");
